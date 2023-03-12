05a8e831b7857bff265b825a3dcd777bd3668b59
Korbin Dansie <dansiekm@gmail.com>
2023-03-12 15:16:40 -0600

    Working on handling errors

---
+++ AirlineReservationSystem/ErrorHandling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservationSystem
+{
+    internal static class ErrorHandling
+    {
+        /// <summary>
+        /// Handel the errro
+        /// </summary>
+        /// <param name="SClass"></param>
+        /// <param name="sMethod"></param>
+        /// <param name="sMessage"></param>
+        public static void handelError(string SClass, string sMethod, string sMessage)
+        {
+            System.IO.File.AppendAllText("C:\\" + Directory.GetCurrentDirectory() + @"\Resources\Error\Error.txt", Environment.NewLine +
+                             "HandleError Exception: " + SClass + "." + sMethod + "->" + sMessage);
+        }
+
+        public static void throwError()
+        {
+
+        }
+    }
+}
